8e6f2e080697c6bab49cabc401db9a2b61ea2cf6
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-01 09:47:16 +0100

    Modify server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express"); //dlaczego var a nie const?
+var express = require("express"); 
 var bodyParser = require("body-parser");
 var fs = require("fs"); //potrzebny do operacji na plikach
 var app = express();
-var stringifyFile;
+var stringifyFile; //przechowuje aktualną zawartość pliku
 app.use(bodyParser.json()); //żeby skorzystać z formatu application/json
 
 app.get("/getNote", function(req, res) {
@@ -14,10 +14,11 @@ app.get("/getNote", function(req, res) {
 });
 
 app.post("/updateNote/:note", function(req, res) {
-	fs.writeFile("./test.json", stringifyFile, function(err) {
-		if (err) throw err;
+	stringifyFile = stringifyFile + req.params.note;
+	fs.writeFile("./test.json", stringifyFile, function(err,data) {
+		if (err) throw err;	
 		console.log("file updated");
-		data = stringifyFile + req.params.note;
+		stringifyFile = data;
 		res.send(data);
 	});
 });
